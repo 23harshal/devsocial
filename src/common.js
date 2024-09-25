9fd11a1d605a7287102c1597bb121b92863dca40
= <=>
2024-09-25 17:47:53 +0530

    first commit

---
+++ src/common.js
@@ -0,0 +1,5 @@
+const passcode = "YiAn0Dq1S7ob7dv2";
+const username = "hh234pp";
+
+const dburl =
+  "mongodb+srv://<db_username>:<db_password>@social.2hvvx.mongodb.net/?retryWrites=true&w=majority&appName=social";

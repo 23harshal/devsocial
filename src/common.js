181cc328e3c06f1686e398fecfd47bfddc12900a
= <=>
2024-09-29 17:12:00 +0530

    remove safety data

--- src/common.js
+++ src/common.js
@@ -1,12 +1,1 @@
 
-<<<<<<< HEAD
-const dburl =
-  "mongodb+srv://hh234pp:YiAn0Dq1S7ob7dv2@social.2hvvx.mongodb.net/?retryWrites=true&w=majority&appName=social";
-
-module.exports = {
-  passcode,
-  username,
-  dburl,
-};
-=======
->>>>>>> 11538b796658a0ec2a3b56bbc236f89e9d0b1f2c

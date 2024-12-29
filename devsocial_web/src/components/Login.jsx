7394a29eab69aa051dd15f05aea020f41842110f
= <=>
2024-12-29 17:59:00 +0530

    cors error resolved

--- devsocial_web/src/components/Login.jsx
+++ devsocial_web/src/components/Login.jsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+
 const Login = () => {
+  const [email, setEmail] = useState("elon@gmail.com");
+  const [password, setPassword] = useState("Elon@123");
+  const [error, setError] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        BASE_URL + "/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log(response);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="card bg-base-300 w-96 shadow-xl justify-center mx-auto mt-16">
       <div className="px-10 pt-10 flex flex-col gap-5 items-center">
@@ -13,7 +37,13 @@ const Login = () => {
             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
-          <input type="email" className="grow " placeholder="Email" />
+          <input
+            type="email"
+            className="grow "
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
 
         <label className="input input-bordered flex items-center gap-2 w-full">
@@ -29,12 +59,19 @@ const Login = () => {
               clipRule="evenodd"
             />
           </svg>
-          <input type="password" className="grow" value="password" />
+          <input
+            type="password"
+            className="grow"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
       </div>
       <div className="card-body items-center text-center">
         <div className="card-actions">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary" onClick={handleLogin}>
+            Login
+          </button>
         </div>
       </div>
     </div>
--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ const app = express();
 require("dotenv").config();
 const connectDB = require("./utils/database");
 const cookieParser = require("cookie-parser");
+var cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
